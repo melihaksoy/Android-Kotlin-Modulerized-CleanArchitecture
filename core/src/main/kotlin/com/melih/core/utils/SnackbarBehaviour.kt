d41b35b659f8abc00ca34e09bd45a112064c8f58
Melih Aksoy <m.aksoy@tmg.nl>
2019-07-03 11:48:08 +0200

    Using SnackbarBehaviour for pushing list when there's an error
    Using SnackbarBehaviour for pushing list when there's an error

--- core/src/main/kotlin/com/melih/core/utils/SnackbarBehaviour.kt
+++ core/src/main/kotlin/com/melih/core/utils/SnackbarBehaviour.kt
@@ -4,18 +4,20 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.snackbar.Snackbar
 
+/**
+ * Simple behaviour for pushing views when snackbar is animating so none of views will remain under snackbar
+ */
 class SnackbarBehaviour constructor(
     context: Context,
     attributeSet: AttributeSet
-) : CoordinatorLayout.Behavior<SwipeRefreshLayout>() {
+) : CoordinatorLayout.Behavior<View>() {
 
-    override fun layoutDependsOn(parent: CoordinatorLayout, child: SwipeRefreshLayout, dependency: View): Boolean =
+    override fun layoutDependsOn(parent: CoordinatorLayout, child: View, dependency: View): Boolean =
         dependency is Snackbar.SnackbarLayout
 
-    override fun onDependentViewChanged(parent: CoordinatorLayout, child: SwipeRefreshLayout, dependency: View): Boolean {
+    override fun onDependentViewChanged(parent: CoordinatorLayout, child: View, dependency: View): Boolean {
         val translationY = Math.min(0.0f, (dependency.translationY - dependency.height))
         child.translationY = translationY
         return true
