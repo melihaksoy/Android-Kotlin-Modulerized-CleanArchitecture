97d552593793b69e3cb32da027cca6bca471f2d3
Melih Aksoy <aksoy.melihcan@gmail.com>
2019-09-26 16:38:21 +0200

    Updated package name

--- features/launches/src/main/kotlin/com/melih/launches/di/modules/LaunchesFragmentModule.kt
+++ features/launches/src/main/kotlin/com/melih/launches/di/modules/LaunchesFragmentModule.kt
@@ -1,9 +1,9 @@
-package com.melih.list.di.modules
+package com.melih.launches.di.modules
 
 import androidx.lifecycle.ViewModel
 import androidx.paging.Config
 import com.melih.core.di.keys.ViewModelKey
-import com.melih.list.ui.vm.LaunchesViewModel
+import com.melih.launches.ui.vm.LaunchesViewModel
 import com.melih.repository.interactors.DEFAULT_LAUNCHES_AMOUNT
 import com.melih.repository.interactors.GetLaunches
 import dagger.Binds
